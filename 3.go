96b42ab02f88e69ed420ab9866a011df0c897266
Burhan Yusuf <burhanyusuf@outlook.com>
2021-04-12 10:57:50 +0700

    Find String Inside Bracket GO

---
+++ 3.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"strings"
+	"fmt"
+)
+
+func findFirstStringInBracket(str string) string { 
+
+	if(str == ""){
+		return "";
+	}
+
+	indexOpenBracket := strings.Index(str,"("); // get index open bracket character
+	indexCloseBracket := strings.Index(str,")"); // get index close bracket character
+
+	if(indexOpenBracket >= 0 && indexCloseBracket >= 0){ // if index open and close found
+		return str[indexOpenBracket+1:indexCloseBracket]; // return string from open+1 untul indexclose
+	}
+
+	return ""; // if not found open bracket char or close bracket
+}
+
+func main() {
+	fmt.Println(findFirstStringInBracket("eee(ef(fff) wowowowwwwww"))
+}
